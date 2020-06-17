6946fabe56529ad22aede0ef176644932f817640
Leonardo <lqfragozo@gmail.com>
2020-06-17 19:19:04 -0300

    Finalizado crud do model de categorias

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,22 @@
 class CategoriesController < ApplicationController
+
+  #5.11 Updating Articles
+
   def index
     @categories = Category.all
   end
 
   def show
     @category = Category.find(params[:id])
+
   end
 
   def new
+    @category = Category.new
+  end
+
+  def edit
+    @category = Category.find(params[:id])
   end
 
   def create
@@ -19,6 +28,23 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+  @category = Category.find(params[:id])
+
+  if @category.update(category_params)
+    redirect_to @category
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @category = Category.find(params[:id])
+  @category.destroy
+
+  redirect_to categories_path
+end
 
   private
     def category_params
