6bfbf2c187ba250394398fa2f71d1fe78d7a5336
Leonardo <lqfragozo@gmail.com>
2020-06-19 14:14:11 -0300

    Rotas resumidas

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'categories/index'
+  devise_for :users
+  root to: 'pages#home'
   resources :categories
   resources :products
-  root to: 'pages#home'
-  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
