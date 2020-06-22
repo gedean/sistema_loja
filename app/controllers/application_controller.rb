96e0b4d0999395c43c67cdcf0ac5a364d877ce69
Leonardo <lqfragozo@gmail.com>
2020-06-22 18:51:09 -0300

    Feita página de 403

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
 before_action :authenticate_user!
+  rescue_from CanCan::AccessDenied do |exception|
+  render file: "#{Rails.root}/public/403", formats: [:html], status: 403, layout: false
+  end
 end
